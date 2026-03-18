2863a297f7e06d7a7ea335a6cd6f07e1520fac49
Arthur Erpen <arthurfonsecaerpen@gmail.com>
2026-03-18 18:42:10 -0300

    initial structure

---
+++ include/semaphore.h
@@ -0,0 +1,38 @@
+// wrapper do semaforo nativo do c para facilitar o uso
+
+// sem o wrapper (C puro):
+// sem_t sem;
+// sem_init(&sem, 0, 0);
+// sem_wait(&sem);
+// sem_post(&sem);
+// sem_destroy(&sem);
+// // com o wrapper:
+// Semaphore sem;
+// sem.p();
+// sem.v();
+// destrutor limpa automaticamente
+
+#ifndef SEMAPHORE_H
+#define SEMAPHORE_H
+
+#include <semaphore.h>
+
+class Semaphore {
+    public:
+        Semaphore(int initial = 0) { // 0 é o valor padrão se não passar parâmetro
+            sem_init(&_sem, 0, initial); // 0 ali indica que o semáforo é compartilhado entre threads do mesmo processo
+        }
+        ~Semaphore() { // chamado automaticamente quando o objeto sair do escopo (quando função termina por ex)
+            sem_destroy(&_sem);
+        }
+        void p() { // decrementa o contador
+            sem_wait(&_sem);
+        }
+        void v() { // incrementa o contador
+            sem_post(&_sem);
+        }
+    private:
+        sem_t _sem;
+};
+
+#endif
