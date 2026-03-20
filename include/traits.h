3c04274287a9d072562f85ff48943fa996a48b29
Arthur Erpen <arthurfonsecaerpen@gmail.com>
2026-03-19 21:16:27 -0300

    create traits class

---
+++ include/traits.h
@@ -0,0 +1,32 @@
+#ifndef TRAITS_H
+#define TRAITS_H
+
+// forward declarations (declarar antecipadamente evita a necessidade de dar #include)
+
+// T sera a engine da NIC (placa de rede)
+template <typename T> class NIC;
+class RawSocketEngine;
+// quando for usar Protocol precisa passar um tipo que represente uma NIC aqui dentro
+template <typename NIC_T> class Protocol;
+
+// template generico vazio
+template <typename T> struct Traits { };
+
+// o que estou fazendo aqui é: quando eu iniciar uma placa de rede NIC baseada em raw socket
+// eu quero essas configurações de numero de buffers e essa interface
+// NIC --> quantos buffers e qual interface
+template <> struct Traits<NIC<RawSocketEngine>> {
+    static const unsigned int SEND_BUFFERS = 8;
+    static const unsigned int RECEIVE_BUFFERS = 16;
+    static constexpr const char* INTERFACE = "eth0";
+};
+
+
+// o que estou fazendo aqui é: quando eu iniciar uma camada de protocolo e ela estiver
+// conectada a placa de rede NIC baseada em raw socket, eu quero que o ethertype seja 0x8888
+// protocol --> qual ethertype usar (0x8888 é livre)
+template <> struct Traits<Protocol<NIC<RawSocketEngine>>> {
+    static const unsigned short ETHERNET_PROTOCOL_NUMBER = 0x8888;
+};
+
+#endif
