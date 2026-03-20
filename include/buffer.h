abe9de684dd16e7dbcd653573abd6bd36cb87bb4
Arthur Erpen <arthurfonsecaerpen@gmail.com>
2026-03-19 21:16:13 -0300

    create buffer class

---
+++ include/buffer.h
@@ -0,0 +1,49 @@
+#ifndef BUFFER_H
+#define BUFFER_H
+
+// template porque a api especificada pelo professor usa Buffer<Ethernet::Frame>
+template<typename T> class Buffer {
+    public:
+    // frame real fica aqui dentro. NIC aloca um buffer, preenche o frame e manda pro engine
+    T* data() {
+        return &_data;
+    }
+    // versao somente pra leitura
+    const T* data() const {
+        return &_data;
+    }
+
+    // tamanho do payload dentro do frame
+    unsigned int size() const {
+        return _size;
+    }
+    // NIC seta quando aloca, protocol le quando recebe
+    void size(unsigned int s) {
+        _size = s;
+    }
+
+    // flag indicando se o buffer ta em uso
+    bool locked() const {
+        return _locked;
+    }
+
+    // marca como indisponivel
+    void lock() {
+        _locked = true;
+    }
+
+    // marca como disponivel
+    void unlock() {
+        _locked = false;
+    }
+
+    private:
+    // frame ethernet em si
+    T _data;
+    // quantos bytes de payload dentro do frame estão sendo usados de fato (o maximo que definimos antes é 1500)
+    unsigned int _size;
+    // se buffer ta livre ou em uso
+    bool _locked;
+};
+
+#endif
