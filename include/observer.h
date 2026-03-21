deae05f793b6df9d105d13af9a5f58c3d729fd3e
caetano borba peruzzo <cbperuzzo4@gmail.com>
2026-03-21 03:00:41 -0300

    feat: conditional data observer pattern

--- include/observer.h
+++ include/observer.h
@@ -12,8 +12,83 @@ template <typename T, typename Condition = void>
 class Conditionally_Data_Observed;
 
 template <typename T, typename Condition = void>
-class Conditional_Data_Observer;
+class Conditional_Data_Observer{
+    friend class Conditionally_Data_Observed<T, Condition>;
+public:
 
+    Conditional_Data_Observer() {}
+
+    ~Conditional_Data_Observer() {}
+
+    void update(Condition c, T * t) {
+        _data.push(t);
+    }
+    T * updated() {
+        T* r = _data.front();
+        _data.pop();
+        return r;
+    }
+private:
+    std::queue<T*> _data:
+};
+
+template <typename T, typename Condition = void>
+class Conditionally_Data_Observed{
+        friend class Conditional_Data_Observer<T, Condition>;
+
+public:
+
+    struct Conditional_Data_Observer_Entry {
+        Conditional_Data_Observer<T, Condition>* observer;
+        Condition condition;
+    };
+    
+    typedef std::vector<Conditional_Data_Observer_Entry> Observers;
+
+public:
+
+    Conditionally_Data_Observed(){
+        _observers.reserve(32);
+    }
+
+    ~Conditionally_Data_Observed() {}
+
+    void attach(Conditional_Data_Observer<T, Condition> * o, Condition c) {
+        _observers.push_back({o, c});
+
+    }
+ 
+    void detach(Conditional_Data_Observer<T, Condition> * o, Condition c) {
+        // tira os elementos que dão true no lambda da range begin - end (a range não é alterada pelo movimento dos elementos)
+        auto it = std::remove_if(_observers.begin(), _observers.end(),
+        [&](const Conditional_Data_Observer_Entry& e) { 
+            return e.observer == o && e.condition == c; 
+        });
+        // it é o priemeiro iterator deposi do end "antigo"
+        // agora apagamos tudo fora da range antiga
+        _observers.erase(it, _observers.end());
+        
+    }
+    
+    bool notify(Condition c, T * d) {
+        // caso seja garantido qeu atach/detach sejam so realizados durante o "startup" seria possível tirar o safe guard, mas como não é
+        bool notified = false;
+        for (auto& e : _observers){
+            if (e.condition == c){
+                e.observer->update(c, d);
+                notified = true;
+            }
+        }
+        return notified;
+        
+    }
+private:
+    Observers _observers;
+    std::mutex _mtx;
+};
+
+
+// both the observer and observed use mutex to be thread safe
 template<typename D, typename C = void>
 class Concurrent_Observed;
 
@@ -59,14 +134,14 @@ class Concurrent_Observed
 
 public:
 
-    struct Entry {
+    struct Concurrent_Observer_Entry {
         Concurrent_Observer<D, C>* observer;
         C condition;
     };
     typedef D Observed_Data;
     typedef C Observing_Condition;
     // *intrusive linked list (mas por agora vector vai funcionar bem)
-    typedef std::vector<Entry> Observers;
+    typedef std::vector<Concurrent_Observer_Entry> Observers;
 
 public:
 
@@ -87,7 +162,7 @@ public:
         _mtx.lock();
         // tira os elementos que dão true no lambda da range begin - end (a range não é alterada pelo movimento dos elementos)
         auto it = std::remove_if(_observers.begin(), _observers.end(),
-        [&](const Entry& e) { 
+        [&](const Concurrent_Observer_Entry& e) { 
             return e.observer == o && e.condition == c; 
         });
         // it é o priemeiro iterator deposi do end "antigo"
