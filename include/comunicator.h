17317b0f7929c251360b679df530b6af87440c90
caetano borba peruzzo <cbperuzzo4@gmail.com>
2026-03-25 21:55:55 -0300

    feat: comunicator

---
+++ include/comunicator.h
@@ -0,0 +1,56 @@
+#ifndef COMUNICATOR_H
+#define COMUNICATOR_H
+
+#include "message.h"
+#include "observers/concurrent_observer.h"
+
+template <typename Channel>
+class Communicator: public Concurrent_Observer<typename Channel::Observer::Observed_Data, typename Channel::Observer::Observing_Condition> {
+
+    typedef Concurrent_Observer<typename Channel::Observer::Observed_Data, typename Channel::Observer::Observing_Condition> Observer;
+
+public:
+    typedef typename Channel::Buffer Buffer;
+    typedef typename Channel::Address Address;
+
+public:
+    Communicator(Channel * channel, Address address): Observer(address.port()), _channel(channel), _address(address) {
+        _channel->attach(this, address.port());
+    }
+
+    ~Communicator() { _channel->detach(this, _address.port()); }
+
+    bool send(const Message * message) {
+        // cout << "Mensagem enviada: " << message->const_data() << endl;
+        return (_channel->send(_address, Channel::Address::BROADCAST, message->const_data(), message->size()) > 0);
+    }
+
+    bool receive(Message * message) {
+        Buffer * buf = Observer::updated(); // block until a notification is triggered
+
+        if (!buf) {
+            print("ERROR: No buffer available");
+            return false;
+        }
+
+        typename Channel::Address from;
+        int size = _channel->receive(buf, &from, message->data(), message->size());
+        message->size(size);
+
+        if (size <= 0)
+            return false;
+        
+        return true;
+    }
+
+private:
+    void update(typename Channel::Observer::Observing_Condition c, Buffer * buf) {
+        Observer::update(c, buf);
+    }
+
+private:
+    Channel * _channel;
+    Address _address;
+};
+
+#endif
