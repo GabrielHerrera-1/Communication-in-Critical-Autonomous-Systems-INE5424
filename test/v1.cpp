aaf21b5eb9b6d953f1434a4d49c3834d1b33b961
caetano borba peruzzo <cbperuzzo4@gmail.com>
2026-03-27 23:56:04 -0300

    feat: makefile

--- test/v1.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/vehicle/vehicle.h"
-
-int main(){
-    Vehicle v((0x0404));
-
-    v.init();
-}
--- test/v2.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/vehicle/vehicle.h"
-
-int main(){
-    Vehicle v((0x8080));
-
-    v.init();
-}
