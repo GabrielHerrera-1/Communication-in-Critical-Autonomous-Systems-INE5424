d3985cabdaca212fec6b056c667ebee4abcdbdcd
Arthur Erpen <arthurfonsecaerpen@gmail.com>
2026-04-08 17:07:10 -0300

    feat: traits adapted to shared memory

--- src/core/traits.h
+++ src/core/traits.h
@@ -6,6 +6,7 @@
 // T sera a engine da NIC (placa de rede)
 template <typename T> class NIC;
 class RawSocketEngine;
+class SharedMemoryEngine;
 // quando for usar Protocol precisa passar um tipo que represente uma NIC aqui dentro
 template <typename NIC_T> class Protocol;
 
@@ -26,6 +27,19 @@ template <> struct Traits<NIC<RawSocketEngine>> {
 // conectada a placa de rede NIC baseada em raw socket, eu quero que o ethertype seja 0x8888
 // protocol --> qual ethertype usar (0x8888 é livre)
 template <> struct Traits<Protocol<NIC<RawSocketEngine>>> {
+    static const unsigned short ETHERNET_PROTOCOL_NUMBER = 0x8888;
+};
+
+// NIC de shared memory: mesmos buffers, sem interface de rede
+template <> struct Traits<NIC<SharedMemoryEngine>> {
+    static const unsigned int SEND_BUFFERS = 8;
+    static const unsigned int RECEIVE_BUFFERS = 16;
+    static constexpr const char* INTERFACE = nullptr;
+};
+
+// protocol sobre shared memory usa o mesmo ethertype
+// (o header do Protocol viaja dentro do frame na SHM também)
+template <> struct Traits<Protocol<NIC<SharedMemoryEngine>>> {
     static const unsigned short ETHERNET_PROTOCOL_NUMBER = 0x8888;
 };
 
